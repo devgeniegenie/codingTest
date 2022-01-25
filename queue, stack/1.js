ca97ef7a5c9ac1893751bfa6f15a4bdbf0848b5b
Genie <genie@cntt.co.kr>
2022-01-25 18:02:19 +0900

    init

---
+++ queue, stack/1.js
@@ -0,0 +1,35 @@
+/**
+ * 2019 카카오 개발자 겨울 인턴십 크레인 인형뽑기 게임 문제
+ * https://programmers.co.kr/learn/courses/30/lessons/64061
+ **/
+
+const solution = (board, moves) => {
+    let stack = [];
+    let result = 0;
+    for (const move of moves) {
+        let index = move - 1
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][index] !== 0) {
+                let temp = board[i][index]
+                board[i][index] = 0
+                if (stack[stack.length - 1] === temp) {
+                    stack.pop()
+                    result += 2
+                } else {
+                    stack.push(temp)
+                }
+                break
+            }
+        }
+    }
+    return result
+}
+
+let board = [[0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1]];
+
+let moves = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(board, moves))
