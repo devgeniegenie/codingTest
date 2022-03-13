f1a5984c1bee10b5f67d7b1f7ac637048eb56c8d
Genie <genie@cntt.co.kr>
2022-03-13 21:58:58 +0900

    연습중

---
+++ 재귀함수, DFS/test.js
@@ -0,0 +1,23 @@
+const sol = (input) => {
+    let result = []
+    let checkArray = new Array(input+1).fill(1)
+    const dfs = n => {
+        if(n === input + 1){
+            let temp = []
+            for(let i = 1; i <= input+1; i++){
+                if(checkArray[i] === 1){
+                    temp.push(i)
+                }
+            }
+            result.push(temp)
+        }else{
+            checkArray[n] = 1
+            dfs(n+1)
+            checkArray[n] = 0
+            dfs(n+1)
+        }
+    }
+    dfs(1)
+    console.log(result)
+}
+sol(4)
